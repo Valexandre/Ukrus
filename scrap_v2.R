b5fb33c5a42c45e993044230945a763382754a33
Victor <Valexandre@users.noreply.github.com>
2022-03-23 17:43:07 +0100

    Update scrap_v2.R

--- scrap_v2.R
+++ scrap_v2.R
@@ -5,7 +5,7 @@ sheetid<-"1S3_Y1-jti8ofVnPumryFjnZr5wo9GrSMCheYj-2IH68"
 touslieux<-"locations"
 couleurs<-"points"
 # designate project-specific cache
-options(gargle_oauth_cache = “.secrets”)
+options(gargle_oauth_cache = ".secrets")
 # check the value of the option, if you like
 gargle::gargle_oauth_cache()
 # trigger auth on purpose to store a token in the specified cache
