579bdc79855ecf9cc0ae0fab107fcbf447911e69
Victor <Valexandre@users.noreply.github.com>
2022-03-23 17:31:51 +0100

    Check

---
+++ scrap_v2.R
@@ -0,0 +1,13 @@
+library(rvest)
+library(jsonlite)
+library(googlesheets4)
+sheetid<-"1S3_Y1-jti8ofVnPumryFjnZr5wo9GrSMCheYj-2IH68"
+touslieux<-"locations"
+couleurs<-"points"
+# designate project-specific cache
+options(gargle_oauth_cache = “.secrets”)
+# check the value of the option, if you like
+gargle::gargle_oauth_cache()
+# trigger auth on purpose to store a token in the specified cache
+# a broswer will be opened
+googlesheets4::sheets_auth()
