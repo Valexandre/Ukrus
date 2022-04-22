5b35b4f475f53cff65d5872497f552194243c35b
Victor <Valexandre@users.noreply.github.com>
2022-04-22 14:44:27 +0200

    Update scrapwiki.R

--- scrapwiki.R
+++ scrapwiki.R
@@ -63,6 +63,8 @@ sortLeJson<-function(date,idversionchoisie){
   textesansbackslach<-gsub("\\t",'',textesansbackslach)
   textesansbackslach<-gsub('\\"','"',textesansbackslach, fixed = TRUE)
   textesansbackslach<-gsub("'",'"',textesansbackslach)
+    textesansbackslach<-gsub("--{",'{',textesansbackslach)
+     
   textesansbackslach<-gsub('\\["\\{','\\[\\{',textesansbackslach)
   jsoncars<-gsub('\\}"\\]','\\}\\]',gsub('\\["\\{','\\[\\{',textesansbackslach%>%jsonlite::toJSON(pretty = TRUE)))
   #On écrit la base des lieux
